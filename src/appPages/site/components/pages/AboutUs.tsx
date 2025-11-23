4577018d98ddc9c3223f66ee8c135d71692d66ca
nurmuhammedjs13 <nurmuhammedaltybaev@gmail.com>
2025-11-23 23:48:21 +0600

    added about us

--- src/appPages/site/components/pages/AboutUs.tsx
+++ src/appPages/site/components/pages/AboutUs.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroAbout from "./aboutUsSection/heroAbout/heroAbout";
+import Owner from "./aboutUsSection/owner/Owner";
 
 function AboutUs() {
-  return (
-    <div>
-      <HeroAbout />
-    </div>
-  );
+    return (
+        <div>
+            <HeroAbout />
+            <Owner />
+        </div>
+    );
 }
 
 export default AboutUs;
