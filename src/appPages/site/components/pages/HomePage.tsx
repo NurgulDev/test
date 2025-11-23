397337778487aa450ac7d4a426dbdf2624776680
nurmuhammedjs13 <nurmuhammedaltybaev@gmail.com>
2025-11-23 16:53:21 +0600

    adede

--- src/appPages/site/components/pages/HomePage.tsx
+++ src/appPages/site/components/pages/HomePage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Main from "./homeSections/main/Main";
 import Cards from "./homeSections/cards/Cards";
 import Why from "./homeSections/why/Why";
+import HomeCourses from "./homeSections/homeCourses/HomeCourses";
 
 function HomePage() {
     return (
@@ -9,6 +10,7 @@ function HomePage() {
             <Main />
             <Cards />
             <Why />
+            <HomeCourses />
         </div>
     );
 }
